b5b94cdb649c60d0cedbd661641216bd6f9937b9
Flávio L. C. de Moura <flaviomoura@unb.br>
2016-05-03 23:19:55 -0300

    deleting files

--- Equation_C.v.d
+++
@@ -1,1 +0,0 @@
-Equation_C.vo Equation_C.glob Equation_C.v.beautified: Equation_C.v ./Metatheory.vo ./LibTactics.vo ./LambdaES_Defs.vo ./Rewriting.vo ./LambdaES_FV.vo ./LambdaES_Infra.vo ./LambdaES_Tac.vo
--- Rewriting.v.d
+++
@@ -1,1 +0,0 @@
-Rewriting.vo Rewriting.glob Rewriting.v.beautified: Rewriting.v ./Metatheory.vo ./LambdaES_Defs.vo
